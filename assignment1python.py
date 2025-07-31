3954763321820ba6042adb5edc2d6658a2f679a4
Sneha20-cloud <23ad10sn59@mitsgwl.ac.in>
2025-07-31 22:40:48 +0530

    Add files via upload

---
+++ assignment1python.py
@@ -0,0 +1,19 @@
+#Task 1 solution
+num1=int(input("enter the first number : "))
+num2=int(input("enter the second number : "))
+add=num1+num2
+print("addition: ",add)
+sub=num1-num2
+print("subtraction: ",sub)
+mul=num1*num2
+print("multiplication:",mul)
+div=num1/num2
+print("division: ",div)
+
+#task 2 solution
+name=input("enter your first name: ")
+surname=input("enter your last name: ")
+
+print("Hello ",name + " " + surname ,' ! welcome to the python program.')
+
+
